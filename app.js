6bcee0e3e726521f7dc83b9af5f6f0d186d68360
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-24 23:48:22 -0600

    inserted some routes in order to view page in local host 3000

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+// var mysql = require("mysql");
+// var connection = require("./connection");
+
+
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+var entries = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/api/entries", function(req, res) {
+    var newentry = req.body;
+    newentry.routeName = newentry.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newentry);
+    entries.push(newentry);
+    res.json(newentry);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
