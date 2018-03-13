475c86bd07efca11df46588ebac96b4327eaf4b6
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-13 15:22:54 -0500

    changed the name of db and table

--- db/schema.sql
+++ db/schema.sql
@@ -6,22 +6,22 @@ DROP DATABASE health_app;
 CREATE DATABASE health_app;
 USE health;
 
-CREATE TABLE users (
-    user_id INT(100) AUTO_INCREMENT,
-    user_fname VARCHAR(40) NOT NULL,
-    user_lname VARCHAR(40) NOT NULL,
-    PRIMARY KEY (user_id)
-);
+-- CREATE TABLE users (
+--     user_id INT(100) AUTO_INCREMENT,
+--     user_fname VARCHAR(40) NOT NULL,
+--     user_lname VARCHAR(40) NOT NULL,
+--     PRIMARY KEY (user_id)
+-- );
 
 CREATE TABLE health (
     event_id INT(100) AUTO_INCREMENT,
-    user_id INT(100) NOT NULL,
+    -- user_id INT(100) NOT NULL,
     run_date DATE,
     total_miles INT(100) NOT NULL,
     total_min INT(100) NOT NULL,
     calorie_intake INT(100) NOT NULL,
     PRIMARY KEY (event_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    -- FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 SELECT *  FROM users
